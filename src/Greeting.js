6240d5dc4e9466701f564e93e2a5df30d48ba23a
Jonah Wambua <djmrjay@gmail.com>
2023-08-16 17:08:13 +0300

    Create the Greeting component

---
+++ src/Greeting.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRandomGreeting } from './store/actions';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const randomGreeting = useSelector(state => state.randomGreeting);
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>{randomGreeting}</h1>
+    </div>
+  );
+};
+
+export default Greeting;
